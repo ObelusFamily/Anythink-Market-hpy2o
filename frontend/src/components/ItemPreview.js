538e0d69b5f6150440544d7c71694018a0f7724a
Bhaskar <bhaskarjyotidas365@gmail.com>
2022-08-05 21:36:55 +0530

    image placed with source

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -38,7 +38,7 @@ const ItemPreview = (props) => {
       <img
         alt="item"
         // src={item.image}
-        src={frontend/placeholder.png}
+        src={image.placeholder}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />

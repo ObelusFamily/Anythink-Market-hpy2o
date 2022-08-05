d85bfd007d0de5cb54247a2fd240a0be33a25317
Bhaskar <bhaskarjyotidas365@gmail.com>
2022-08-05 22:12:57 +0530

    image placed with source

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import agent from "../agent";
 import { connect } from "react-redux";
 import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
-import { image } from './../../public/placeholder.png';
+import image from "../../public/placeholder.png";
 
 const mapDispatchToProps = (dispatch) => ({
   favorite: (slug) =>
@@ -38,7 +38,7 @@ const ItemPreview = (props) => {
       <img
         alt="item"
         // src={item.image}
-        src={image.placeholder}
+        src={image}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
